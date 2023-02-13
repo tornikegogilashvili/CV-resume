0aba15b47ce5969560a7f628fb94ac3b0ecc60cb
tornike gogilashvili <tornikegogilashvili@yahoo.com>
2023-02-13 23:03:28 +0400

    redberry

--- js/update2.js
+++ js/update2.js
@@ -2,4 +2,6 @@ const forma = document.querySelector("piradi_form");
 
 forma.addEventListener("submit", (es)=>{
     es.preventDefault();
+    const fd = new FormData(forma);
+    console.log(fd);
 })
