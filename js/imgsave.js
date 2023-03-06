6bc1c12addafb6c03a54437bba55d222b4c4ae00
tornike gogilashvili <tornikegogilashvili@yahoo.com>
2023-03-06 22:45:51 +0400

    CV

--- js/imgsave.js
+++ js/imgsave.js
@@ -1,40 +1,46 @@
-            // const fileEl = document.querySelector(".img");
-            // const btn = document.querySelector(".piradi_next");
+            const fileEl = document.querySelector(".img");
+            const btn = document.querySelector(".piradi_next");
             
-            // fileEl.addEventListener("change", ()=>{
-            //     const fr = new FileReader();
-            //     fr.readAsDataURL(fileEl.files[0]);
-            //     fr.addEventListener("load", ()=>{
-            //         const url = fr.result;
-            //         const img = new Image();
-            //         img.src = url;
-            //         let child = document.querySelector(".image_CV");
-            //         child.src=url;
-            //     });
-            // });
+            fileEl.addEventListener("change", ()=>{
+                const fr = new FileReader();
+                fr.readAsDataURL(fileEl.files[0]);
+                fr.addEventListener("load", ()=>{
+                    const url = fr.result;
+                    const img = new Image();
+                    img.src = url;
+                    let child = document.querySelector(".image_CV");
+                    child.src=url;
+                });
+            });
 
-            // btn.addEventListener("click", ()=>{
-            //                 window.location.href = "gamocdileba.html";
+            btn.addEventListener("click", ()=>{
+                            window.location.href = "gamocdileba.html";
 
-            //             })
+                        })
 
 
-const fileEl = document.querySelector(".img");
-fileEl.addEventListener("change", function () {
+// const fileEl = document.querySelector(".img");
+// fileEl.addEventListener("change", function () {
 
-    const wamkitxveli = new FileReader();
+//     const wamkitxveli = new FileReader();
 
-    wamkitxveli.addEventListener("load", ()=>{
-        localStorage.setItem("recent-image", wamkitxveli.result);
-    });
-    wamkitxveli.readAsDataURL(this.files[0]);
+//     wamkitxveli.addEventListener("load", ()=>{
+//         localStorage.setItem("recent-image", wamkitxveli.result);
+//     });
+//     wamkitxveli.readAsDataURL(this.files[0]);
     
-});
+// });
     
 
-const json5 = localStorage.getItem("recent-image");
-console.log(json5);
-document.querySelector(".image_CV").setAttribute("src", json5)
+// const json5 = localStorage.getItem("recent-image");
+// console.log(json5);
+// document.querySelector(".image_CV").src=json5;
+
+
+
+
+
+
 
 // document.addEventListener("DOMContentLoaded", () => {
 //     const recentImageDataUrl = localStorage.getItem("recent-image");
