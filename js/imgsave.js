05b423b28dd41a14c6462bd16b54b24612c6e463
tornike gogilashvili <tornikegogilashvili@yahoo.com>
2023-03-06 20:45:06 +0400

    CV

--- js/imgsave.js
+++ js/imgsave.js
@@ -1,17 +1,40 @@
-            const fileEl = document.querySelector(".img");
-            const btn = document.querySelector(".piradi_next");
-            btn.addEventListener("click", ()=>{
-                window.location.href = "gamocdileba.html";
+            // const fileEl = document.querySelector(".img");
+            // const btn = document.querySelector(".piradi_next");
+            
+            // fileEl.addEventListener("change", ()=>{
+            //     const fr = new FileReader();
+            //     fr.readAsDataURL(fileEl.files[0]);
+            //     fr.addEventListener("load", ()=>{
+            //         const url = fr.result;
+            //         const img = new Image();
+            //         img.src = url;
+            //         let child = document.querySelector(".image_CV");
+            //         child.src=url;
+            //     });
+            // });
 
-            })
-            fileEl.addEventListener("change", ()=>{
-                const fr = new FileReader();
-                fr.readAsDataURL(fileEl.files[0]);
-                fr.addEventListener("load", ()=>{
-                    const url = fr.result;
-                    const img = new Image();
-                    img.src = url;
-                    let child = document.querySelector(".image_CV");
-                    child.src=url;
-                });
-            });
+            // btn.addEventListener("click", ()=>{
+            //                 window.location.href = "gamocdileba.html";
+
+            //             })
+
+
+const fileEl = document.querySelector(".img");
+fileEl.addEventListener("change", function () {
+
+    const wamkitxveli = new FileReader();
+
+    wamkitxveli.addEventListener("load", ()=>{
+        localStorage.setItem("recent-image", wamkitxveli.result);
+    });
+    wamkitxveli.readAsDataURL(this.files[0]);
+    
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const recentImageDataUrl = localStorage.getItem("recent-image");
+    
+        document.querySelector(".image_CV").setAttribute("src", recentImageDataUrl)
+    
+})
+            
